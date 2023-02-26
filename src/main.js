a20943aee000158a5da34e80e3334d6303bd8edc
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2023-02-26 21:00:38 +0600

    Rearranging js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.querySelector(".click-me");
 const code = document.querySelector(".code");
-const codeSpan = document.querySelector(".code span");
+const codeSpan = code.querySelector("span");
 const r = document.querySelector(":root");
+const notification = document.querySelector(".notification");
+const notificationColor = notification.querySelector(".notification-color");
+const notificationText = notification.querySelector("p");
 
 function getRandomNumber() {
     return Math.floor(Math.random() * hex.length);
@@ -22,7 +25,15 @@ btn.addEventListener("click", () => {
     document.body.style.backgroundColor = colorCode;
     btn.style.backgroundColor = colorCode;
     r.style.setProperty("--shadow", `0 0 55px 1px ${colorCode}`);
-    code.innerHTML = `Hex Code: <span style="background-color: ${colorCode}" onclick="copyCode(this.textContent)">${colorCode}<i class="fa-regular fa-copy"></i></span>`;
+    codeSpan.innerHTML = `${colorCode}<i class="fa-regular fa-copy"></i>`;
+    codeSpan.style.backgroundColor = colorCode;
+    notificationColor.style.backgroundColor = colorCode;
+    notificationText.textContent = `${colorCode} copied to clipboard!`;
 });
 
+codeSpan.addEventListener("click", (e) => copyCode(e.target.textContent));
 
+function copyCode(text) {
+    navigator.clipboard.writeText(text);
+    console.log("copied");
+}
